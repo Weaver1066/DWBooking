57126ef48de77d3c24fbf40fb6d8a9143259346f
NoFail-osu <75129270+NoFail-osu@users.noreply.github.com>
2022-05-03 12:13:08 +0200

    user code first

--- DWBooking/Model/User.cs
+++ DWBooking/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace DWBooking.Model
 {
     public class User
     {
+        [Key]
+        [StringLength(50)]
         public string Username { get; set; }
+        
+        [Required]
+        [StringLength(100)]
         public string Password { get; set; }
 
         public User()
--- DWBooking/Services/UserService.cs
+++ DWBooking/Services/UserService.cs
@@ -9,13 +9,22 @@ namespace DWBooking.Services
     public class UserService
     {
         public List<User> Users { get; set; }
-
+        public GenericDbService<User> DbService { get; set; }
+        
         public UserService()
         {
             
         }
 
+        public void AddUser(Employee i)
+        {
+            Users.Add(new User(i.Username, i.Password));
+        }
 
+        public List<User> GetUsers()
+        {
+            return Users;
+        }
 
     }
 }

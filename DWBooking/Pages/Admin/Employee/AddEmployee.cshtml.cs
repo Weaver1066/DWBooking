765f298f089996d83f3fd9f4a02a8bbcde50dae6
Arian Zanjani <aria0127@edu.easj.dk>
2022-05-03 11:26:21 +0200

    liaf

--- DWBooking/Pages/Admin/Employee/AddEmployee.cshtml.cs
+++ DWBooking/Pages/Admin/Employee/AddEmployee.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DWBooking.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,30 @@ namespace DWBooking.Pages.Admin.Employee
 {
     public class AddEmployeeModel : PageModel
     {
-        public void OnGet()
+        private EmployeeService employeeService;
+        private List<Model.Employee> employees;
+
+        [BindProperty]
+        public Model.Employee Employee { get; set; }
+
+        public AddEmployeeModel(EmployeeService iS)
         {
+            employeeService = iS;
+            employees = employeeService.GetEmployees().ToList();
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            employeeService.AddEmployee(Employee);
         }
     }
 }
--- DWBooking/Services/EmployeeService.cs
+++ DWBooking/Services/EmployeeService.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DWBooking.Model;
 
 namespace DWBooking.Services
 {
     public class EmployeeService
     {
+        public List<Employee> Employees { get; set; }
+
+        public void AddEmployee(Employee employee)
+        {
+            Employees.Add(employee);
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return Employees;
+        }
+        // Returnere listen af employees
+
+        //public Employee GetEmployee(string employeeId)
+        //{
+        //    foreach (Employee employee in Employees)
+        //    {
+        //        if (employee.Id == employeeId) return employee;
+        //    }
+
+        //    return null;
+        //}
+
+
     }
 }

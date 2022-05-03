b6430c5a8e896f09217f173de794bb2fad01a8e9
Weaver1066 <patr509g@edu.zealand.dk>
2022-05-03 10:39:59 +0200

    db step 1

---
+++ DWBooking/Model/DWBookingDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DWBooking.Model
+{
+    public class DWBookingDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DWSystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Paticipant> Paticipants { get; set; }
+        public DbSet<Counceling> Councelings { get; set; }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
--- DWBooking/Services/GenericDbService.cs
+++ DWBooking/Services/GenericDbService.cs
@@ -2,10 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DWBooking.Interface;
 
 namespace DWBooking.Services
 {
-    public class GenericDbService
+    public class GenericDbService<T> : Iservice<T> where T : class
     {
+        public async Task<IEnumerable<T>> GetObjectsAsync()
+        {
+            using (var context = new VagtPlanDbContext())
+            {
+                return await context.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+
+        public async Task AddObjectAsync(T obj)
+        {
+            using (var context = new VagtPlanDbContext())
+            {
+                context.Set<T>().Add(obj);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteObjectAsync(T obj)
+        {
+            using (var context = new VagtPlanDbContext())
+            {
+                context.Set<T>().Remove(obj);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public Task UpdateObjectAsync(T obj)
+        {
+            using (var context = new VagtPlanDbContext())
+            {
+
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetObjectByIdAsync(int id)
+        {
+            using (var context = new VagtPlanDbContext())
+            {
+                context.Set<T>().FindAsync(id);
+                await context.SaveChangesAsync();
+            }
+
+            throw new Exception("no id found");
+        }
     }
 }

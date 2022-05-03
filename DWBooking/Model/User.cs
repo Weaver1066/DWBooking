5f785990bebd4be8bb80b104b0302316e1d3307c
NoFail-osu <75129270+NoFail-osu@users.noreply.github.com>
2022-05-03 11:31:04 +0200

    user næsten done

--- DWBooking/Model/User.cs
+++ DWBooking/Model/User.cs
@@ -7,5 +7,18 @@ namespace DWBooking.Model
 {
     public class User
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            
+        }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
     }
 }
--- DWBooking/Services/UserService.cs
+++ DWBooking/Services/UserService.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DWBooking.Model;
 
 namespace DWBooking.Services
 {
     public class UserService
     {
+        public List<User> Users { get; set; }
+
+        public UserService()
+        {
+            
+        }
+
+
+
     }
 }

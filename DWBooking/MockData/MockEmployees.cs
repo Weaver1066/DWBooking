000179cfed6dbccef4996f49e0a49c6a27c3eefa
Weaver1066 <patr509g@edu.zealand.dk>
2022-05-09 10:58:20 +0200

    change

--- DWBooking/MockData/MockEmployees.cs
+++ DWBooking/MockData/MockEmployees.cs
@@ -10,9 +10,9 @@ namespace DWBooking.MockData
     {
         private static List<Employee> employees = new List<Employee>()
         {
-            new Employee("Weaver Patrick", "84920341", "WeaverP@Outlook.dk", new DateTime(1995, 10, 2), "Englandsvej 10", 1, 20),
-            new Employee("Torben Støger", "72839103", "TStøger@Outlook.dk", new DateTime(1985, 1, 28), "Haggisvej 20", 2, 21),
-            new Employee("Anette Pedersen", "32930142", "AP@Outlook.dk", new DateTime(1997, 5, 8), "Københavnsvej 5", 3, 22)
+            new Employee("Weaver Patrick", "84920341", "WeaverP@Outlook.dk", new DateTime(1995, 10, 2), "Englandsvej 10", 1, new User("admin","admin")),
+            new Employee("Torben Støger", "72839103", "TStøger@Outlook.dk", new DateTime(1985, 1, 28), "Haggisvej 20", 2, new User("tester", "tester")),
+            new Employee("Anette Pedersen", "32930142", "AP@Outlook.dk", new DateTime(1997, 5, 8), "Københavnsvej 5", 3)
         };
 
         public static List<Employee> GetMockEmployees()

dc3a5e083e46848acfd7bd46269e7f9b2e1bef9f
NoFail-osu <75129270+NoFail-osu@users.noreply.github.com>
2022-05-09 10:47:45 +0200

    add client / councelling x view table thing idek anymore please lord and savior take y life

--- DWBooking/Pages/Admin/Clients/AddClient.cshtml.cs
+++ DWBooking/Pages/Admin/Clients/AddClient.cshtml.cs
@@ -12,31 +12,43 @@ namespace DWBooking.Pages.Admin.Clients
     public class AddClientModel : PageModel
     {
         private CouncelingService councelingService;
+        private ClientService clientService;
 
+        [BindProperty]
         public  Model.Counceling Counceling { get; set; }
-        public  Model.Client Client { get; set; }
-        public List<Model.Counceling> Councelings { get; private set; }
+
+        [BindProperty]
+        public Model.Client Client { get; set; }
 
 
-        public AddClientModel(CouncelingService councelingService) //Dependency Injection
+        public AddClientModel(CouncelingService councelingService, ClientService clientService) //Dependency Injection
         {
             this.councelingService = councelingService;
+            this.clientService = clientService;
         }
         public IActionResult OnGet()
         {
             return Page();
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPost()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            councelingService.AddCouncelingAsync(Counceling);
-            return RedirectToPage("GetAllCounceling");
+            if (clientService.CheckPhone(Client.Phone) == null)
+            {
+                await clientService.AddClientAsync(Client);
+                await councelingService.AddCouncelingAsync(Counceling);
+                return RedirectToPage("GetAllCounceling");
+            }
+            else
+            {
+                await councelingService.AddCouncelingAsync(Counceling);
+                return RedirectToPage("GetAllCounceling");
+            }
         }
     }
-    
 }
